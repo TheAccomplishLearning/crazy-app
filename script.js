272ece2e3a6449cf9deff08460891b06900ee21c
TheAccomplishLearning <80941425+TheAccomplishLearning@users.noreply.github.com>
2023-05-06 07:05:19 -0700

    1 file modified

--- script.js
+++ script.js
@@ -1,4 +1,13 @@
 console.log('Hello!');
 
 let body = document.querySelector('body');
-body.classList.add('light-mode');
+// body.classList.add('light-mode');
+changeMode('light-mode');
+
+// indexedDB
+
+function changeMode(mode) {
+  if ((mode = 'light-mode')) body.classList.add(mode);
+  else if ((mode = 'dark-mode')) body.classList.add(mode);
+  else return;
+}
